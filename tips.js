118505fb6d90c44c5024651ea17f861387c6807f
CharKwehTiawKerang <takamagahara16@gmail.com>
2023-02-08 18:40:21 +0800

    Guess what? I added some IMAGES in proton files and some new engines

--- tips.js
+++ tips.js
@@ -17,7 +17,6 @@ xvi) ADDITIONAL INFORMATION / MISCELLANEOUS TIPS
 
 //Perdana 2022 - same with honda accord spare parts
 //Toyota Avanza air filter = produa kembara
-//testing woi
 
 //Most spare parts car item can be used for machine such as gear oil, spark plugs etc.
 - Bosch WS8E Spark Plug = Chainsaw Machine (mesin rumput, mesin potong pokok, pam racun)
